5a33ab3eec81b99e37121478080958d08cceacd7
Hwan-Joon Choi <hc5duke@gmail.com>
2011-07-18 23:10:05 -0700

    validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_many :services
 
   attr_accessible :handle, :pushup_set_count
+
+  validate :pushup_set_count, :numericality => true, :greater_than_or_equal_to => 0, :less_than => 10000
 end
