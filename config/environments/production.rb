158b1554f4b8ca80dddd046f693fa81f1e482314
Hwan-Joon Choi <hc5duke@gmail.com>
2011-07-18 23:50:22 -0700

    need auth stuff in production too

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,8 @@ Buffset::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    require 'openid/store/filesystem'
+    provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  end
 end
