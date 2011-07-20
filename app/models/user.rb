a1b8a8b761d3c1e82d785341ac4d4de5d6025ce3
Amir Manji <amanji75@gmail.com>
2011-07-20 00:24:56 -0700

    fix validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   attr_accessible :handle, :pushup_set_count, :active
 
-  validate :pushup_set_count, :numericality => true, :greater_than_or_equal_to => 0, :less_than => 10000
+  validate :pushup_set_count, :numericality => true, :greater_than_or_equal_to => 0
   validate :handle, :length => { :minimum => 1, :maximum => 10}
 
   scope :active, :conditions => [ 'active = ?', true ], :order => [ 'handle' ]
